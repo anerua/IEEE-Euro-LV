6e996b43d81539e2b269c6ece62d7122356c0f89
Martins Anerua <aneruamartins@gmail.com>
2021-10-19 08:52:31 +0100

    init function

--- Data_Generator.py
+++ Data_Generator.py
@@ -7,3 +7,15 @@ Created on Tue Oct 19 08:10:06 2021
 """
 
 
+import pandas as pd
+
+
+class Data_Generator:
+
+    def __init__(self) -> None:
+        
+        data = pd.DataFrame(columns=['Day', 'time', 'Disco', 'Backup', 'PV(kW)', 'Battery_state', 'Battery_percent', 'Total_load_demand', 'Price_multiplier'])
+
+    
+    def add_entry(self):
+        self.data
