429ad52b738f449115df488ac9c0775a728b020e
Martins Anerua <aneruamartins@gmail.com>
2021-10-19 09:22:49 +0100

    can add an entry and also save entire data as csv

--- Data_Generator.py
+++ Data_Generator.py
@@ -12,10 +12,28 @@ import pandas as pd
 
 class Data_Generator:
 
-    def __init__(self) -> None:
+    def __init__(self, length, start_time, end_time) -> None:
         
-        data = pd.DataFrame(columns=['Day', 'time', 'Disco', 'Backup', 'PV(kW)', 'Battery_state', 'Battery_percent', 'Total_load_demand', 'Price_multiplier'])
+        self.length = length
+        self.start_time = start_time
+        self.end_time = end_time
+        self.data = pd.DataFrame(columns=['Day', 'Time', 'Disco', 'Backup', 'PV(kW)', 'Battery_state', 'Battery_percent', 'Total_load_demand', 'Price_multiplier'])
 
     
-    def add_entry(self):
-        self.data
+    def add_entry(self, entry):
+        
+        self.data.append({
+            'Day': entry['day'],
+            'Time': entry['time'],
+            'Disco': entry['disco'],
+            'Backup': entry['backup'],
+            'PV(kW)': entry['pv'],
+            'Battery_state': entry['bat_state'],
+            'Battery_percent': entry['bat_percent'],
+            'Total_load_demand(kW)': entry['total_load_demand'],
+            'Price_multiplier': entry['price_mult']
+        }, ignore_index=True)
+
+    
+    def save_data(self):
+        self.data.to_csv("test.csv")
